ecf87c3143249ceb5622e15aa54f6886f11ab5a7
Nick Roberts <narobert@Nick-Robertss-MacBook-Pro-9.local>
2015-08-26 23:15:10 -0700

    integrating vk api

---
+++ hiplyst/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    url(r'^$', 'vkconfig.views.dashboard', name='dashboard'),
+    url(r'^admin/', include(admin.site.urls)),
+
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
