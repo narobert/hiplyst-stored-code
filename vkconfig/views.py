02013c604c97ddde42b4efcf65f0cc422e13afd2
Nick Roberts <narobert@Nick-Robertss-MacBook-Pro-9.local>
2015-08-27 01:04:18 -0700

    integrating vk api

--- vkconfig/views.py
+++ vkconfig/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def dashboard(request):
-    return render_to_response("dashboard.html", {"request": request});
+    return render_to_response("dashboard.html");
