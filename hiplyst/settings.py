802784d1cfa16992431e412a802a108fb989d0f7
Nick Roberts <narobert@Nick-Robertss-MacBook-Pro-9.local>
2015-08-27 02:46:45 -0700

    integrating vk api

--- hiplyst/settings.py
+++ hiplyst/settings.py
@@ -93,7 +93,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'hiplyst/static'),
 )
 
 TEMPLATE_DIRS = (
