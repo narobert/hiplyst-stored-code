6f2b81134d794df6d58282965688b2af47565662
Nick Roberts <narobert@Nick-Robertss-MacBook-Pro-9.local>
2015-09-07 15:47:26 -0700

    integrating vk api

--- vkconfig/migrations/0001_initial.py
+++ vkconfig/migrations/0001_initial.py
@@ -15,10 +15,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Playlist',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(serialize=False, verbose_name=b'#', primary_key=True)),
                 ('playlist_name', models.CharField(max_length=100)),
                 ('num_songs', models.IntegerField()),
                 ('total_duration', models.IntegerField()),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
@@ -29,26 +30,15 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('url', models.CharField(max_length=1000)),
-                ('aid', models.IntegerField()),
                 ('title', models.CharField(max_length=1000)),
                 ('artist', models.CharField(max_length=1000)),
-                ('duration', models.IntegerField()),
                 ('genre', models.CharField(max_length=40)),
+                ('duration', models.IntegerField()),
+                ('aid', models.IntegerField()),
+                ('playlist', models.ForeignKey(to='vkconfig.Playlist')),
             ],
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='playlist',
-            name='track',
-            field=models.ManyToManyField(to='vkconfig.Track', blank=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='playlist',
-            name='user',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
-            preserve_default=True,
         ),
     ]
